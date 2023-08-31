d82aee1fbd512c5180e13392cf6510e8c864b3f7
lucashrv <lucas.hrv@outlook.com>
2023-08-31 12:57:30 -0300

    feat: errors yup & translation pt-br

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -8,11 +8,37 @@ import {
 
 interface City {
     name: string
+    state: string
 }
 
 const bodyValidation: ObjectSchema<City> = object({
-    name: string().required().min(3)
+    name: string().required().min(3),
+    state: string().required().min(3),
 })
+
+// export const createBodyValidator: RequestHandler = async (req, res, next) => {
+//     const data: City = {
+//         ...req.body
+//     }
+
+//     try {
+
+//         validateData = await bodyValidation.validate(data)
+
+//         return res.json(data)
+
+//     } catch (error) {
+
+//         const yupError = error as ValidationError
+
+//         return res.status(500).json({
+//             errors: {
+//                 default: yupError.message
+//             }
+//         })
+
+//     }
+// }
 
 export const create = async (req: Request<{}, {}, City>, res: Response) => {
 
@@ -20,23 +46,26 @@ export const create = async (req: Request<{}, {}, City>, res: Response) => {
         ...req.body
     }
 
-    let validateData: City | undefined = undefined
+    let validatedData: City | undefined = undefined
 
     try {
 
-        validateData = await bodyValidation.validate(data)
+        validatedData = await bodyValidation.validate(data, { abortEarly: false })
 
-        return res.json(data)
+        return res.json(validatedData)
 
-    } catch (error) {
+    } catch (err) {
 
-        const yupError = error as ValidationError
+        const yupError = err as ValidationError
+        const errors: Record<string, string> = {}
 
-        return res.status(500).json({
-            errors: {
-                default: yupError.message
-            }
+        yupError.inner.forEach(error => {
+            if (!error.path) return
+
+            errors[error.path] = error.message
         })
+
+        return res.status(400).json({ errors })
 
     }
 }
